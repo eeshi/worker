b4e873faba3f764de33e249c09677670b34e774e
adrianObel <obeladrian@gmail.com>
2014-02-13 15:38:00 -0430

    Added secret file

--- crawler.js
+++ crawler.js
@@ -1,1 +1,7 @@
 var casper = require('casper')
+	, secret = require('./secret')
+
+casper.create()
+
+var siteA  = require('./resources/crawl-site-a')(casper, secret)
+
---
+++ resources/crawl-site-a.js
@@ -0,0 +1,14 @@
+var casper
+	, secret
+  , crawler = {}
+
+exports.module = function(_casper, _secret) {
+
+	casper = casper || _casper
+	secret = secret || _secret
+	return crawler
+}
+
+crawler.crawl = function() {
+	
+}
