8875f23ef28a7364916da1b49549538e3aabdd64
adrianObel <obeladrian@gmail.com>
2014-02-13 23:37:37 -0430

    Added userAgent, logging, and disabled loading images

--- crawler.js
+++ crawler.js
@@ -1,7 +1,16 @@
-var casper = require('casper')
+var casper = require('casper').create({
+    verbose: true
+  , logLevel: 'error'
+  , userAgent: 'Mozilla/5.0 (Windows NT 6.2; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/32.0.1667.0 Safari/537.36'
+  , pageSettings: {
+      loadImages: false
+    } 
+  })
+
   , secret = require('./secret')
+  , siteA  = require('./resources/crawl-site-a')(casper, secret)
 
-casper.create()
+siteA.crawl()
 
-var siteA  = require('./resources/crawl-site-a')(casper, secret)
 
+casper.run()
