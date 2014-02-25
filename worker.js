2546030974c7e5f57a83f26d58809c069d339ed3
adrianObel <obeladrian@gmail.com>
2014-02-24 23:34:25 -0430

    Remote call to saveJobs on every page crawled - Needs propper fixing - Commented out for the momment

--- worker.js
+++ worker.js
@@ -13,50 +13,60 @@ var number = process.env.NUMBER;
 var d = dnode.connect(PORT);
 var concurrency = 8;
 var worker = {};
-var jobs = [];
+var remote = {};
 
 worker.id = number;  
 
 
-d.on('remote', function onRemote(remote) {
+d.on('remote', function onRemote(_remote) {
+  remote = _remote;
   remote.initWorker(worker, initWorker);
   remote.getJobs(worker, getJobs);
 });
 
 function initWorker(server) {
-  console.log(server);
+  console.log(server.response);
 }
 
 function getJobs(site) {
   
   var url = site.url + site.section;
+  var $;
+
+  // Shorten url to avoid console spam
+  var shortUrl = url.trim().slice(0,42);
   console.log(url)
 
-  request(site.url, function servePage(err, res, body) {
+  request(url, function servePage(err, res, body) {
     
     if (err) throw err;
 
-    var $ = cheerio.load(body);
-    
-    var shortTitle = $('title').text().trim().slice(0,42);
-    console.log(shortTitle);
+    $ = cheerio.load(body);
 
-    var jobsOnThisPage = $(site.selectors.job).map(function() {
-      return $(this).attr('href');
-    });
-
-    jobs.push(jobsOnThisPage);
-
-    console.log(site.selectors.nextPage);
+    var jobsOnThisPage = $(site.selectors.job).map(getHrefFromLink);
 
     var nextSection = $(site.selectors.nextPage).attr('href');
 
+    worker.jobsCollected = jobsOnThisPage;
+
+    // TODO:
+    // Fix transport to save jobs collected
+    //remote.saveJobs(worker, saveJobs);
+
     // Call recursively until last page
     if(nextSection) {
-      var newUrl = site.url + nextSection;
-      getJobs(newUrl);
+      site.section = nextSection;
+      getJobs(site);
     }
 
     return;
   });
+
+  function getHrefFromLink() {
+    return $(this).attr('href');
+  }
+
+  function saveJobs(server) {
+    console.log(server.response);
+  }
 }
