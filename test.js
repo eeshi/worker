018cea149de11b222bc074d2b565cf08a2507457
stefanmaric <alexstefan92@gmail.com>
2014-04-07 20:55:36 -0430

    Using models instead.

--- test.js
+++ test.js
@@ -1,71 +1,56 @@
 var MongoClient = require('mongodb').MongoClient;
 var api = require('./api.js');
-var fs = require('fs');
-var url;
-var model;
-var options;
+var models = require('./models.json');
 
-// Computrabajo example
+models.forEach(function(item, i, arr) {
+  crawlLinks(item, i, arr)
+});
 
-url = 'http://www.computrabajo.com.ve/bt-ofrd-dismetro-0.htm';
-model = {
-  pageTitle: 'title',
-  title: 'td[align=center] > font[size=2] > b',
-  desc: 'td[colspan=2] > p[align=justify] font[size=2]',
-  links: {
-    selector: 'a[href="/"]',
-    get: 'href',
-    multi: true
-  }
-};
-options = {
-  requestOptions: {
-    encoding: 'binary'
-  },
-  cheerioOptions: {}
-};
+function crawlLinks(item, i, arr) {
 
-
-// Bumeran example
-
-// url = 'http://www.bumeran.com.ve/empleos/ingeniero-civil-de-campo-y-tsu-en-construccion-civil-muros-y-o-pantallas-atirantadas-proyectos-y-construcciones-sabpi-c.a.-1001707776.html';
-// model = {
-//   pageTitle: 'title',
-//   title: 'h2.h2',
-//   desc: '#contenido_aviso'
-// };
-// options = {};
+  var url = item.prot + item.baseUrl + item.linksList.startUrl;
+  var model = item.linksList.model;
+  var options = item.linksList.options;
 
 
 
-api.scrape(url, model, options, function(err, data) {
+  api.scrape(url, model, options, function(err, data) {
 
-  if (err) {
-    return console.error(err);
-  }
+    if (err) {
+      return console.error(err);
+    }
 
-  data.url = url;
+    data.url = url;
 
-  console.log(data);
+    console.log(data);
 
-  mongoDB(function(db) {
-    
-    var jobPosts = db.collection('jobPosts');
-    jobPosts.insert(data, function(err, docs) {
-      
-      if(err) {
-        console.log(err);
-      }
-      db.close();
+    mongoDB(function(db) {
+
+      var jobLinks = db.collection('jobLinks');
+      jobLinks.insert(data, function(err, docs) {
+
+        if(err) {
+          console.log(err);
+        }
+
+        db.close();
+
+        if (data.nextPageLink) {
+          item.linksList.startUrl = data.nextPageLink;
+          return crawlLinks(item, i, arr);
+        }
+
+
+      });
+
     });
 
   });
-
-});
+}
 
 
 function mongoDB(callback) {
-  
+
   MongoClient.connect(process.env.DATABASE_URL, function(err, db) {
     if(err) {
       console.log(err);
